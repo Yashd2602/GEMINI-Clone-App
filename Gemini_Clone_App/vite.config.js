bc3bb40fd77c86d98a114fb0db719d5708d17169
Yashd2602 <yashdubey2602@gmail.com>
2025-06-22 10:09:55 +0530

    Assets updated

--- Gemini_Clone_App/vite.config.js
+++ Gemini_Clone_App/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@google/generative-ai'],
+  },
   base: command === 'serve'
     ? '/'
     : '/GEMINI-Clone-App/',  // 🛑 Replace with your repo name
